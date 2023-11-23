72b8ab602208188d878b44d116868570b3c83b6f
MichalSzmajduch <m.szmajduch1996@gmail.com>
2023-11-23 20:17:42 +0100

    final homework

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,98 @@
+import React, { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+
+import { ContactForm } from './contactForm/ContactForm';
+import { Filter } from './filter/Filter';
+import { ContactList } from './contactList/ContactList';
+
+import css from './app.module.css';
+
+const initialContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
 export const App = () => {
+  const [contacts, setContacts] = useState(initialContacts);
+
+  const [filter, setFilter] = useState('');
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  useEffect(() => {
+    const storedContacts = localStorage.getItem('contacts');
+    if (!storedContacts) {
+      const storedContacts = JSON.stringify(initialContacts);
+      localStorage.setItem('contacts', storedContacts);
+    }
+  }, []);
+
+  useEffect(() => {
+    const existingContacts = localStorage.getItem('contacts');
+    const contacts = JSON.parse(existingContacts);
+
+    if (contacts !== null) {
+      setContacts(contacts);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (contacts !== initialContacts) {
+      const changeInContacts = JSON.stringify(contacts);
+      localStorage.setItem('contacts', changeInContacts);
+    }
+  }, [contacts]);
+
+  const addContactOnSubmit = ({ name, number }) => {
+    const contactOnList = contacts.some(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
+    if (contactOnList) {
+      alert('This contact is already on Your list');
+    } else {
+      const newContact = {
+        id: nanoid(),
+        name,
+        number,
+      };
+
+      setContacts([...contacts, newContact]);
+    }
+  };
+
+  const deleteContact = contactId => {
+    const remainingContacts = contacts.filter(
+      contact => contact.id !== contactId
+    );
+    setContacts([...remainingContacts]);
+  };
+
+  const onFilterChange = event => {
+    event.preventDefault();
+    setFilter(event.target.value.toLowerCase());
+  };
+
+  const showFilteredContact = () => {
+    return contacts.filter(contact => {
+      return contact.name.toLowerCase().includes(filter);
+    });
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.container}>
+      <h1>Phonebook</h1>
+      <ContactForm
+        onSubmit={addContactOnSubmit}
+        name={name}
+        number={number}
+        setName={setName}
+        setNumber={setNumber}
+      />
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={onFilterChange} />
+      <ContactList contacts={showFilteredContact()} onDelete={deleteContact} />
     </div>
   );
 };
---
+++ src/components/contactForm/ContactForm.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
+
+import PropTypes from 'prop-types';
+
+import css from './contactForm.module.css';
+
+export const ContactForm = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const nameId = nanoid();
+  const numberId = nanoid();
+
+  const handleSubmit = evt => {
+    evt.preventDefault();
+
+    if (!name || !number) {
+      return;
+    }
+
+    onSubmit({ name, number });
+
+    reset();
+  };
+
+  const reset = () => {
+    setName('');
+    setNumber('');
+  };
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+    if (name === 'name') {
+      setName(value);
+    } else if (name === 'number') {
+      setNumber(value);
+    }
+  };
+
+  return (
+    <>
+      <form className={css.form} onSubmit={handleSubmit}>
+        <label className={css.label} htmlFor={nameId}>
+          Name
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={handleChange}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            placeholder="name"
+          ></input>
+        </label>
+        <label className={css.label} htmlFor={numberId}>
+          Number
+          <input
+            type="tel"
+            name="number"
+            value={number}
+            onChange={handleChange}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            placeholder="number"
+          />
+        </label>
+
+        <button type="submit">Add contact</button>
+      </form>
+    </>
+  );
+};
+
+ContactForm.propTypes = {
+  name: PropTypes.string,
+  number: PropTypes.string,
+  onSubmit: PropTypes.func.isRequired,
+};
